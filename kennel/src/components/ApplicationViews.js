67fc5ca1b7dd089e524a4e9a018056e86e55bb99
Blake Davis <blavis21@gmail.com>
2019-07-19 21:21:01 -0500

    refactored state to empty arrays, moved objects into json file

--- kennel/src/components/ApplicationViews.js
+++ kennel/src/components/ApplicationViews.js
@@ -7,48 +7,30 @@ import OwnerList from './owner/OwnerList'
 
 
 export default class ApplicationViews extends Component {
-    employeesFromAPI = [
-        { id: 1, name: "Jessica Younker" },
-        { id: 2, name: "Jordan Nelson" },
-        { id: 3, name: "Zoe LeBlanc" },
-        { id: 4, name: "Blaise Roberts" }
-    ]
-
-    locationsFromAPI = [
-        { id: 1, name: "Nashville North", address: "500 Circle Way" },
-        { id: 2, name: "Nashville South", address: "10101 Binary Court" }
-    ]
-
-    animalsFromAPI = [
-        { id: 1, name: "Doodles" },
-        { id: 2, name: "Jack" },
-        { id: 3, name: "Angus" },
-        { id: 4, name: "Henley" },
-        { id: 5, name: "Derkins" },
-        { id: 6, name: "Checkers" }
-    ]
-
-    ownersFromAPI = [
-        { id: 1, name: "Bob", phoneNumber: "615-111-1111" },
-        { id: 2, name: "Susie", phoneNumber: "615-111-1112" },
-        { id: 3, name: "Hank", phoneNumber: "615-111-1113" },
-        { id: 4, name: "Beth", phoneNumber: "615-111-1114" },
-    ]
-
-
-
-    // If you haven't created the owners array in your state yet, please go ahead and create it now, and populate it with 4 owners. Each owners should have the id, phoneNumber, and name properties.
-
-    // Create a link in your navigation bar that links to /owners path.
-    // Create a route for /owners that renders the <OwnerList> component and sends the corresponding state property.
-    // Add the code in <OwnerList> to display all the items in the array.
-
 
     state = {
-        employees: this.employeesFromAPI,
-        locations: this.locationsFromAPI,
-        animals: this.animalsFromAPI,
-        owners: this.ownersFromAPI
+        employees: [],
+        locations: [],
+        animals: [],
+        owners: []
+    }
+
+    componentDidMount() {
+        const newState = {}
+    
+        fetch("http://localhost:5002/animals")
+            .then(r => r.json())
+            .then(animal => newState.animals = animal)
+            .then(() => fetch("http://localhost:5002/employees")
+            .then(r => r.json()))
+            .then(employee => newState.employees = employee)
+            .then(() => fetch("http://localhost:5002/locations"))
+            .then(r => r.json())
+            .then(location => newState.locations = location)
+            .then(() => fetch("http://localhost:5002/owners"))
+            .then(r => r.json())
+            .then(owner => newState.owners = owner)
+            .then(() => this.setState(newState))
     }
 
     render() {
